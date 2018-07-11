a32f044b6168d692928f87f3aa745109605a05e2
VaisOlga <15_3_vays@mail.ru>
2018-07-11 13:33:42 +0500

    Create VoteFactory.sol

---
+++ contracts/VoteFactory.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.19;
+
+contract VoteFactory {
+    
+    address owner;
+    
+    struct Vote {
+        string question;
+        string[] answers;
+    }
+    
+    Vote[] public votes;
+    mapping (uint => address) voteToOwner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function createVote(string _question) public {
+        uint voteId = votes.push(Vote(_question, new string[](0))) - 1;
+        voteToOwner[voteId] = msg.sender;
+    }
+    
+    function addAnswer(uint _voteId, string _answer) public {
+        require(voteToOwner[_voteId] == msg.sender);
+        votes[_voteId].answers.push(_answer);
+    }
+
+    function voteAnswer(uint _voteId, uint _answerId) public view returns(string) {
+        return votes[_voteId].answers[_answerId];
+    }
+}
